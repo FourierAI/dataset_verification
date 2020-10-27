3265ae73d5502b8da9667bcec16eaecdeb442d25
Zhipeng Ye <t-zhipengye@microsoft.com>
2020-10-27 09:40:53 +0800

    modify code style

--- main.py
+++ main.py
@@ -3,22 +3,22 @@ import json
 
 
 def verify_single_classification(input_filepath):
-    output_content = []
     error_lines = []
+    error_lines_index = []
     labels = set()
     with open(input_filepath) as file:
         for index, line in enumerate(file):
             contents = line.split('\t')
             labels.add(contents[0])
             if not len(contents) == 2:
-                output_content.append(line)
-                error_lines.append(str(index + 1))
-    return output_content, error_lines, list(labels)
+                error_lines.append(line)
+                error_lines_index.append(str(index + 1))
+    return error_lines, error_lines_index, list(labels)
 
 
 def verify_multi_classification(input_filepath):
-    output_content = []
     error_lines = []
+    error_lines_index = []
     labels = set()
     with open(input_filepath) as file:
         for index, line in enumerate(file):
@@ -26,39 +26,39 @@ def verify_multi_classification(input_filepath):
             label_list = contents[0].split(',')
             labels.add(label_list)
             if '' in label_list or not len(contents) == 2:
-                output_content.append(line)
-                error_lines.append(str(index + 1))
-    return output_content, error_lines, list(labels)
+                error_lines.append(line)
+                error_lines_index.append(str(index + 1))
+    return error_lines, error_lines_index, list(labels)
 
 
 def verify_named_entity_recognition(input_filepath):
-    output_content = []
     error_lines = []
+    error_lines_index = []
     labels = set()
     with open(input_filepath) as file:
         for index, line in enumerate(file):
             try:
                 jsobj = json.loads(line)
             except Exception:
-                output_content.append(line)
-                error_lines.append(str(index + 1))
+                error_lines.append(line)
+                error_lines_index.append(str(index + 1))
             if jsobj and not ('raw_text' in jsobj and 'entities' in jsobj):
-                output_content.append(line)
-                error_lines.append(str(index + 1))
+                error_lines.append(line)
+                error_lines_index.append(str(index + 1))
 
             if jsobj and 'entities' in jsobj and len(jsobj['entities']) > 0:
                 for entity in jsobj['entities']:
                     labels.add(entity['class_name'])
 
-    return output_content, error_lines, list(labels)
+    return error_lines, error_lines_index, list(labels)
 
 
-def dump_result(output_filepath, output_content, error_lines, labels):
+def dump_result(output_filepath, error_lines, error_lines_index, labels):
     json_obj = {}
-    if len(output_content) > 0:
+    if len(error_lines) > 0:
         json_obj['result'] = 'failure'
-        json_obj['error lines'] = output_content
-        json_obj['error lines index'] = error_lines
+        json_obj['error lines'] = error_lines
+        json_obj['error lines index'] = error_lines_index
     else:
         json_obj['result'] = 'successful'
     json_obj['labels'] = labels
@@ -66,8 +66,7 @@ def dump_result(output_filepath, output_content, error_lines, labels):
         json.dump(json_obj, file, ensure_ascii=False)
 
 
-if __name__ == '__main__':
-
+def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--input')
     parser.add_argument('--output')
@@ -77,12 +76,14 @@ if __name__ == '__main__':
     input_filepath = args.input
     output_filepath = args.output
     dataset_type = args.type
-
     if dataset_type == 'SC':
-        output_content, error_lines, labels = verify_single_classification(input_filepath)
+        error_lines, error_lines_index, labels = verify_single_classification(input_filepath)
     elif dataset_type == 'MC':
-        output_content, error_lines, labels = verify_multi_classification(input_filepath)
+        error_lines, error_lines_index, labels = verify_multi_classification(input_filepath)
     else:
-        output_content, error_lines, labels = verify_named_entity_recognition(input_filepath)
+        error_lines, error_lines_index, labels = verify_named_entity_recognition(input_filepath)
+    dump_result(output_filepath, error_lines, error_lines_index, labels)
 
-    dump_result(output_filepath, output_content, error_lines, labels)
+
+if __name__ == '__main__':
+    main()
